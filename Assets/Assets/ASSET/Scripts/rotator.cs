8f046b3656ecf7f63c3448be369ea53cbe0a487c
BirchTree <albertchi1205@gmail.com>
2021-01-20 13:19:47 -0800

    Unity2020BFS
    Updated to Unity2020

--- Assets/Assets/ASSET/Scripts/rotator.cs
+++ Assets/Assets/ASSET/Scripts/rotator.cs
@@ -6,7 +6,7 @@ public class rotator : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 45, 0) * Time.deltaTime);
 
 		
 	}
---
+++ Assets/Scripts/DestroyBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBox : MonoBehaviour
+{
+
+
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider impactObject)
+	{
+		if (impactObject.tag != "Box")
+		{
+
+			Instantiate(explosion, transform.position, transform.rotation);
+          
+			Destroy(transform.parent.parent.gameObject);
+		}
+
+	}
+	void OnTriggerEnter3D(Collider col)
+	{
+		//LevelControlScript.instance.youWin ();
+	}
+
+}
